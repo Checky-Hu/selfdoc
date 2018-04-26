fa091dbf19415731f0a6892f5d999e9727fbd666
Checky-Hu <checkywho@gmail.com>
2018-04-26 16:12:31 +0800

    add cpp test:pass 0 to std::vector and call data()

---
+++ test/cpp/std_vector_init.cc
@@ -0,0 +1,20 @@
+// Try to pass 0 to this cmd.
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 2) {
+    std::cout << "invalid parameters\n";
+    return -1;
+  }
+
+  int num = 0;
+  std::stringstream ss;
+  ss << argv[1];
+  ss >> num;
+  std::vector<int> data(num);
+  std::cout << "pointer=" << data.data() << "\n";
+  return 0;
+}

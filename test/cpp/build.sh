91ca52f7111c8d9b5d437a16813d093555f270b4
Checky-Hu <checkywho@gmail.com>
2018-12-08 18:43:45 +0800

    single directory for c++14 support test

--- test/cpp/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-clang++ -std=c++17 -stdlib=libc++ -Wall cpp14_support_test.cc -nostdinc++ -isystem /home/checky/Templates/prefix/include/c++/v1/ -lc++ -Wl,-rpath=/home/checky/Templates/prefix/lib/ -o out/clang_cpp14_support_test 

00fbef1fdfeb49082c2eaa21ca910e1e39a3b6a5
Checky-Hu <checkywho@gmail.com>
2018-12-10 15:54:06 +0800

    use "-cxx-isystem" to set cxx header files path instead of system default path

--- test/cpp/cpp14/host_build.sh
+++ test/cpp/cpp14/host_build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clang++ -std=c++17 -stdlib=libc++ -Wall cpp14_support_test.cc -nostdinc++ -isysroot /home/checky/Templates/prefix/ -iwithsysroot /include/c++/v1/ -Wl,-rpath=/home/checky/Templates/prefix/lib/ -fuse-ld=lld -v -o ../out/clang_cpp14_support_test
+clang++ -std=c++17 -stdlib=libc++ -Wall cpp14_support_test.cc -nostdinc++ -cxx-isystem /home/checky/Templates/prefix/include/c++/v1/ -Wl,-rpath=/home/checky/Templates/prefix/lib/ -fuse-ld=lld -v -o ../out/clang_cpp14_support_test

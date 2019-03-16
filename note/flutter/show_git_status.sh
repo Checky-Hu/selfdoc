04288c470b8b0d3485bb4368693a64654102922e
Checky-Hu <checkywho@gmail.com>
2019-03-16 16:26:13 +0800

    modify script file for more git info

--- note/flutter/show_git_status.sh
+++ note/flutter/show_git_status.sh
@@ -5,6 +5,7 @@ git_paths=`find engine -name ".git"`
 git_paths_array=(${git_paths})
 for git_path in ${git_paths_array[*]}
 do
+  echo -e "\n"${git_path}":"
   cd ${git_path}/../
   git status
   cd - > /dev/null
